0eefc903d40f5c7a738ead4e735752aeef377dc4
Penugonda <sundara.k.penugonda@accenture.com>
2023-03-04 13:53:06 +0530

    modified

--- history.js
+++ history.js
@@ -5,11 +5,21 @@ console.log(typeof (historys));
 let write = document.querySelector(".displayhistory")
 let count = 1
 
-write.innerHTML =
-    historys.map((data) => `
+document.querySelector(".clear").addEventListener("click", clearfun)
+function clearfun(){
+    console.log("hi"),  
+    localStorage.removeItem("history"),
+    write.innerHTML=""
+    console.log(localStorage.getItem("history"))
+    console.log("helo");
+}
+
+
+write.innerHTML = historys.map((data) => `
                             <div class="inner">
                                 ${count}. ${data.search}
                                 <span>Searched On: ${data.date} at ${data.time}</span>
-                            </div>
-                                ` )
+                            </div> `)
+
+
 
--- script.js
+++ script.js
@@ -15,24 +15,23 @@ function searchnow() {
         var searchval = document.querySelector(".search").value
         console.log(data);
         let dateandtime=new Date()
-        let obj={
-            search: searchval,
-            date: dateandtime.toLocaleDateString(),
-            time: dateandtime.toLocaleTimeString()
-        }
-        console.log(obj);
+        
 
         
         
         console.log(searchval);
         document.querySelector(".text").innerHTML=`Book Results for '${searchval}'`
+        let count=true
+        let arr=[]
         for(let i=0;i<10;i++){
             //console.log("hi");
             //console.log(data[i].volumeInfo.title);
             //console.log(data[i].volumeInfo.title.includes(searchval));
             if(data[i].volumeInfo.title.includes(searchval)===true){               
-                //console.log(data[i]);
-                //console.log("data");
+                console.log(data[i], typeof(data[i]));
+              
+                arr.push(data[i])
+                console.log(arr);
                 print.innerHTML+=`                 
                     <div class="main">
                         <img src=${data[i].volumeInfo.imageLinks.thumbnail}>
@@ -44,9 +43,17 @@ function searchnow() {
             }
         }
 
+        let obj={
+            search: searchval,
+            date: dateandtime.toLocaleDateString(),
+            time: dateandtime.toLocaleTimeString(),
+            values: arr
+        }
+
         let searchhistory=JSON.parse(localStorage.getItem("history"))
         console.log(searchhistory, typeof(searchhistory));
-        
+       
+        console.log(obj);
 
         if(searchhistory){
             searchhistory.push(obj)
@@ -58,8 +65,14 @@ function searchnow() {
             console.log(searchhistory);
         }
         localStorage.setItem("history", JSON.stringify(searchhistory))
-        window.location.href="./history.html"
+       
         console.log(JSON.parse(localStorage.getItem("history")));
+        document.querySelector(".direct").addEventListener("click",direct)
+        
+        function direct(){
+            console.log("hi");
+            window.location.href="./history.html"
+        }
         
     }
 }
